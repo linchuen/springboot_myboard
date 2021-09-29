9febf6b9511b96788fb9fe59f2278fe598ec4250
Aiden <x29587629@gmail.com>
2021-09-29 21:42:57 +0800

    add react view controll

--- viewController.java
+++
@@ -1,9 +0,0 @@
-package Land.Development.Agency.myboard;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class viewController {
-
-}
